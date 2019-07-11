ad033a4855b40efd1f0e713adaf313deddd90e43
Hanna Lee <mani21na@gmail.com>
2019-07-11 16:02:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,5 +12,3 @@ end
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-
-introduction_with_language_optional ("mani", "Java")
