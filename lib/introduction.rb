c3c6a8aac32ff317d44651b3fcbe3a64a2510ec0
Hanna Lee <mani21na@gmail.com>
2019-07-11 15:55:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@ def introduction (name)
   puts "Hi, my name is #{name}.\n"
 end
 
-introduction ("Josh")
+#introduction ("Josh")
 
 #introduction_with_language
 def introduction_with_language (name, language)
