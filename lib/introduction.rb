8b42c54bf01ce90102e002b4b6da8537a4b10b13
Hanna Lee <mani21na@gmail.com>
2019-07-11 15:44:14 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,15 @@
+#introduction
+def introduction (name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+introduction ("Josh")
+
+#introduction_with_language
+def introduction_with_language (name, language)
+  puts = "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language ("Dan", "Ember.js")
+
+#introduction_with_language_optional
